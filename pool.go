37a4c352ca98f698cee1d6024c4042b9ee10d8ab
Erik Rasmus Nilsson <rasmus@csec.group>
2022-01-15 15:10:06 +0100

    Added pool options for construction instead of several parameters as well as validation regarding pool capacity.

--- pool.go
+++ pool.go
@@ -1,24 +1,46 @@
 package gopewl
 
 import (
-	"fmt"
+	"errors"
 )
 
 // Job is a simple function that takes no arguments and returns no data. This is the basic operation that can be
 // scheduled into the worker pool.
 type Job func()
 
-// Pool manages the Job channel and the workers. Currently, there is not much meaning in keeping references to the
-// workers, but it is kept here as a way to easily expand the pool with additional functionality requiring knowledge
-// of the running workers.
+// Pool manages the Job channel and the workers. It ensures that the number of workers are as expected and spawns
+// or kills workers as needed.
 type Pool struct {
+	capacity int
 	workers []worker
 	queue chan Job
 }
 
 // Schedule sends the provided Job through to the workers.
 func (p *Pool) Schedule(job Job) {
+	if p.allWorkersAreOccupied() && p.canAddWorker() {
+		p.addWorker()
+	}
 	p.queue <- job
+}
+
+func (p *Pool) addWorker() {
+	w := newWorker(p.queue)
+	p.workers = append(p.workers, w)
+	go w.run()
+}
+
+func (p Pool) allWorkersAreOccupied() bool {
+	for _, worker := range p.workers {
+		if worker.waiting {
+			return false
+		}
+	}
+	return true
+}
+
+func (p Pool) canAddWorker() bool {
+	return p.capacity > 0 && p.capacity < len(p.workers)
 }
 
 // Close simply closes the Job channel, which in turn kills all the worker threads.
@@ -26,23 +48,41 @@ func (p *Pool) Close() {
 	close(p.queue)
 }
 
+type PoolOptions struct {
+	poolSize int
+	poolCapacity int
+	queueSize int
+}
+
+func (po PoolOptions) validate() error {
+	if po.poolSize <= 0 {
+		return errors.New("pool size must be a positive integer")
+	}
+	if po.queueSize < 0 {
+		return errors.New("queue size must be a positive integer or 0")
+	}
+	if po.poolCapacity > 0 && po.poolCapacity < po.poolSize {
+		return errors.New("pool capacity must be larger than pool size or 0")
+	}
+	if po.poolCapacity < 0 {
+		return errors.New("pool capacity must be a positive integer or 0")
+	}
+	return nil
+ }
+
 // NewPool returns a Pool with the amount of workers specified in `poolSize` and a queue with the buffer size
 // of `queueSize`
-func NewPool(poolSize int, queueSize int) (*Pool, error) {
-	if poolSize <= 0 {
-		return nil, fmt.Errorf("invalid pool size '%d'", poolSize)
+func NewPool(opts PoolOptions) (*Pool, error) {
+	if err := opts.validate(); err != nil {
+		return nil, err
 	}
-	if queueSize < 0 {
-		return nil, fmt.Errorf("invalid queue size '%d'", queueSize)
+	p := &Pool{
+		capacity: opts.poolCapacity,
+		workers: make([]worker, 0),
+		queue: make(chan Job, opts.queueSize),
 	}
-	pool := Pool{
-		workers: make([]worker, poolSize),
-		queue: make(chan Job, queueSize),
+	for i := 0; i < opts.poolSize; i++ {
+		p.addWorker()
 	}
-	for i := range pool.workers {
-		w := newWorker(pool.queue)
-		pool.workers[i] = w
-		go w.run()
-	}
-	return &pool, nil
+	return p, nil
 }
--- worker.go
+++ worker.go
@@ -3,13 +3,17 @@ package gopewl
 // worker holds a reference to the Job channel and continuously listens for jobs to complete.
 type worker struct {
 	queue chan Job
+	waiting bool
 }
 
 // run is run as a goroutine, and its only responsibility is to read jobs from the Job channel and execute
 // them.
-func (w worker) run() {
+func (w *worker) run() {
+	w.waiting = true
 	for job := range w.queue {
+		w.waiting = false
 		job()
+		w.waiting = true
 	}
 }
 
@@ -17,5 +21,6 @@ func (w worker) run() {
 func newWorker(queue chan Job) worker {
 	return worker{
 		queue: queue,
+		waiting: false,
 	}
 }
